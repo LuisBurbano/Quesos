a4708b1bbf2a334ad8afee2ce54e72144dea211f
Luis Ariel Burbano Pacheco <laburbano1@espe.edu.ec>
2024-03-11 10:19:15 -0500

    regster

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,8 +1,57 @@
 import './Register.css';
 import imgUser from '../assets/img/user.png';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 const Register = (props) => {
+        const [usuario, setUsuario] = useState('');
+        const [nombre, setNombre] = useState('');
+        const [apellido, setApellido] = useState('');
+        const [email, setEmail] = useState('');
+        const [password, setPassword] = useState('');
+        const [error, setError] = useState('');
+    
+        const handleUsuarioChange = (event) => {
+            setUsuario(event.target.value);
+        };
+    
+        const handleNombreChange = (event) => {
+            setNombre(event.target.value);
+        };
+    
+        const handleApellidoChange = (event) => {
+            setApellido(event.target.value);
+        };
+    
+        const handleEmailChange = (event) => {
+            setEmail(event.target.value);
+        };
+    
+        const handlePasswordChange = (event) => {
+            setPassword(event.target.value);
+        };
+    
+        const handleSubmit = async (event) => {
+            event.preventDefault();
+    
+            try {
+                const response = await axios.post('localhost:3000/login', {
+                    EMAILUSUARIO: email,
+                    PASSUSUARIO: password
+                });
+    
+                // Manejar la respuesta si es necesario
+    
+            } catch (error) {
+                console.error('Error al registrar usuario:', error);
+                setError('Error al intentar registrarse');
+            }
+        };  
     return(
+        <>
+        <Navbar/>
         <div className="register">
         <div className="titulo">
             <h1>Registro</h1>
@@ -36,7 +85,9 @@ const Register = (props) => {
                 <input type="submit" value="Registrarse" onclick="validateRegisterForm()"/>
             </div>
         </form>
-    </div>    
+    </div>  
+    <Footer/>  
+    </>
     )
 }
 
